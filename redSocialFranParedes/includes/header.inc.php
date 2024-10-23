e964b794b5b2c98aaf5433e5891f083d2738689d
kfran <117746405+kfran697@users.noreply.github.com>
2024-10-23 16:43:51 +0200

    primeros pasos

---
+++ redSocialFranParedes/includes/header.inc.php
@@ -0,0 +1,11 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Catálogo</a></li>
+            <li><a href="">Sobre Nosotros</a></li>
+            <li><a href="">Carrito</a></li>
+            <li><a href="">Cuenta</a></li>
+        </ul>
+    </nav>
+</header>
